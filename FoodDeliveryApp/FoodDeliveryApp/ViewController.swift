4445f8a64be5baa305e19593bd9d56a2b4ee7a0f
d4tim <seerlaken@gmail.com>
2024-02-01 00:29:25 +0200

    Create a new project, removed storyboard references

---
+++ FoodDeliveryApp/FoodDeliveryApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodDeliveryApp
+//
+//  Created by Дмитрий Тимаров on 01.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+
+
+}
+
